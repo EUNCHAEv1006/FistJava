1a404f9531066c046be564ad370c4c86b2154ee3
김은채 <ke808762@gmail.com>
2023-10-16 12:02:03 +0900

    Study: Type of parameter (basic, references)

--- src/week03/Main.java
+++ src/week03/Main.java
@@ -20,27 +20,5 @@ public class Main {
         System.out.println();
         car.carSpeed(110, 120, 150);
 
-//        // 초기값 기본 값 확인 : 초기값을 준 것은 그 값이 들어가고, 아닌 것은 default value가 set
-//
-//        System.out.println("car.mode = " + car.model);
-//        System.out.println("car.color = " + car.color);
-//        System.out.println();
-//
-//        System.out.println("car.speed = " + car.speed);
-//        System.out.println("car.gear = " + car.gear);
-//        System.out.println("car.lights = " + car.lights);
-//        System.out.println();
-//
-//        System.out.println("car.tire = " + car.tire);
-//        System.out.println("car.door = " + car.door);
-//
-//        // 필드 사용
-//        car.color = "blue"; // 필드 color에 "blue" 데이터를 저장
-//        car.speed = 100;
-//        car.lights = false;
-//
-//        System.out.println("car.color = " + car.color);
-//        System.out.println("car.speed = " + car.speed);
-//        System.out.println("car.lights = " + car.lights);
     }
 }
