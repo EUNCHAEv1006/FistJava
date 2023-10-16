f6639a7ba3d2375c4ef186c920e170c8959eb4ac
김은채 <ke808762@gmail.com>
2023-10-16 11:09:49 +0900

    Study: Create an object

--- src/week03/Main.java
+++ src/week03/Main.java
@@ -2,6 +2,26 @@ package week03;
 
 public class Main {
     public static void main(String[] args) {
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//
+//        System.out.println(car1); // week03.Car@75b84c92 <= car1의 주소
 
+        Car[] carArray = new Car[3];
+        Car car1 = new Car();
+        car1.changeGear('P');
+        carArray[0] = car1;
+
+        Car car2 = new Car();
+        car2.changeGear('N');
+        carArray[1] = car2;
+
+        Car car3 = new Car();
+        car3.changeGear('D');
+        carArray[2] = car3;
+
+        for (Car car: carArray) {
+            System.out.println("car.gear = " + car.gear);
+        }
     }
 }
